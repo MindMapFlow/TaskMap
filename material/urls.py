8d65a517af51f2c551b961dec44a6d615d770d4e
SNOWNUMB <mamayev.stas@gmail.com>
2025-05-11 12:58:00 +0500

    Update Files

--- material/urls.py
+++ material/urls.py
@@ -5,9 +5,6 @@ from .views import theory_view
 urlpatterns = [
     path('', views.index, name='index_old'),
     path('archive-material/', views.archive_material, name='archive_material'),
-    path('achievements/', views.theory_view, name='theory_view'),  # должно вести на theory_view
-path('material/', views.theory_view, name='main_material'),  # если хочешь использовать имя main_material
-
+    path('achievements/', views.theory_view, name='theory_view'),
+    path('material/', views.theory_view, name='main_material'),
 ]
-
-
